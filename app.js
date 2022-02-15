a90de98f79a2edcb3128acead5edc539493a8f67
Georges KABBOUCHI <georges.kabbouchi@gmail.com>
2022-02-15 14:05:25 +0200

    wip

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 import { create } from 'ipfs-core'
+import WebRTCStar from 'libp2p-webrtc-star'
+import wrtc from 'wrtc'
 import prompt from 'prompt'
 
 prompt.message = '';
@@ -7,7 +9,7 @@ prompt.delimiter = '';
 const app = async () => {
     const node = await create({
         repo: '.repo/demo-' + Date.now(),
-        // silent: true,
+        silent: true,
 
         config: {
             Addresses: {
@@ -15,20 +17,26 @@ const app = async () => {
                     '/ip4/0.0.0.0/tcp/0',
                     '/ip4/0.0.0.0/tcp/0/ws',
                     '/dns4/wrtc-star1.par.dwebops.pub/tcp/443/wss/p2p-webrtc-star/',
-                    // '/dns4/star.thedisco.zone/tcp/9090/wss/p2p-webrtc-star',
-                    // '/dns6/star.thedisco.zone/tcp/9090/wss/p2p-webrtc-star',
+                    '/dns4/wrtc-star2.sjc.dwebops.pub/tcp/443/wss/p2p-webrtc-star/'
                 ]
             },
-
             Bootstrap: [
                 "/dnsaddr/bootstrap.libp2p.io/ipfs/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN",
                 "/dnsaddr/bootstrap.libp2p.io/ipfs/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa",
                 // '/dns6/ipfs.thedisco.zone/tcp/4430/wss/p2p/12D3KooWChhhfGdB9GJy1GbhghAAKCUR99oCymMEVS4eUcEy67nt',
                 // '/dns4/ipfs.thedisco.zone/tcp/4430/wss/p2p/12D3KooWChhhfGdB9GJy1GbhghAAKCUR99oCymMEVS4eUcEy67nt'
             ],
-
-            Swarm: {
-                EnableRelayHop: true
+        },
+        libp2p: {
+            modules: {
+                transport: [ WebRTCStar],
+            },
+            config: {
+                transport: {
+                    [WebRTCStar.prototype[Symbol.toStringTag]]: {
+                        wrtc
+                    }
+                }
             }
         }
     })
@@ -40,12 +48,12 @@ const app = async () => {
     });
 
     await node.pubsub.subscribe('message', (msg) => {
-        if(! username){
+        if (!username) {
             return;
         }
 
         const decoded = JSON.parse(new TextDecoder().decode(msg.data));
-     
+
         if (username === decoded.from) {
             return;
         }
