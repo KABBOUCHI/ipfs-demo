6acded4411013d7131fb540174e2d33236152341
Georges KABBOUCHI <georges.kabbouchi@gmail.com>
2022-02-16 23:46:45 +0200

    Update app.js

--- app.js
+++ app.js
@@ -16,67 +16,11 @@ import prompt from 'prompt'
 prompt.message = '';
 prompt.delimiter = '';
 
-const libp2pBundle = (opts) => {
-    // Set convenience variables to clearly showcase some of the useful things that are available
-    const peerId = opts.peerId
-    const bootstrapList = opts.config.Bootstrap
-    const addressesList = opts.config.Addresses.Swarm;
-
-    // Build and return our libp2p node
-    // n.b. for full configuration options, see https://github.com/libp2p/js-libp2p/blob/master/doc/CONFIGURATION.md
-    return Libp2p.create({
-        peerId,
-        addresses: {
-            listen: addressesList
-        },
-        modules: {
-            transport: [WebrtcStar, TCP, Websockets],
-            streamMuxer: [Mplex],
-            connEncryption: [NOISE],
-            peerDiscovery: [MDNS, Bootstrap],
-            pubsub: Gossipsub
-        },
-        config: {
-            transport: {
-                [WebrtcStar.prototype[Symbol.toStringTag]]: {
-                    wrtc
-                }
-            },
-            peerDiscovery: {
-                autoDial: true, // Auto connect to discovered peers (limited by ConnectionManager minConnections)
-                // The `tag` property will be searched when creating the instance of your Peer Discovery service.
-                // The associated object, will be passed to the service when it is instantiated.
-                [Bootstrap.tag]: {
-                    interval: 1000,
-                    enabled: true,
-                    list: bootstrapList
-                },
-                [MDNS.tag]: {
-                    interval: 5000,
-                    enabled: true,
-                }
-            }
-        },
-        peerStore: {
-            persistence: true,
-        },
-    })
-}
-
 const app = async () => {
 
     const node = await create({
         repo: '.repo/demo-' + Date.now(),
         silent: false,
-
-        libp2p: {
-            config: {
-                dht: {
-                    enabled: true,
-                    clientMode: true,
-                },
-            },
-        },
         config: {
             Addresses: {
                 Swarm: [
@@ -90,7 +34,6 @@ const app = async () => {
                 '/ip4/95.179.131.73/tcp/8001/p2p/Qma3Ma763RzXvJzrcfEmqf25QED3rGyKLirVKWvg5Z6pTA',
             ]
         },
-        // libp2p: libp2pBundle
     })
 
     const { username } = await prompt.get({
